7751c9b8b87304acf578820baae7e3893157ef43
ThomMoerman <thomas.moerman.7@gmail.com>
2023-06-20 11:23:23 +0200

    VercelTest

--- api/index.php
+++ api/index.php
@@ -1,23 +1,3 @@
-<?php
- include '../backend/functions.php'
+<?php 
+    echo 'Hello World';
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Contact Support Form</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body class="bg-neutral-50 font-mono">
-    <?php
-        include '../views/header.php'
-    ?>
-    <div class="text-center">
-    <h2 class="text-4xl my-6">Contact Support</h2>
-    <?php
-        include 'form.php'
-    ?>
-    <script src="../backend/validation.js"></script>
-</body>
-</html>
